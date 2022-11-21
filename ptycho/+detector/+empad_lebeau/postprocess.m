133ed5f7e537ec38548c15a6edcd1a7c26c5692d
Colin Gilgenbach <colin@gilgenbach.net>
2022-11-20 19:05:07 -0500

    Updated bg_sub. Now takes bool or float, which is a factor of peak intensity to subtract

--- ptycho/+detector/+empad_lebeau/postprocess.m
+++ ptycho/+detector/+empad_lebeau/postprocess.m
@@ -15,7 +15,13 @@ detStorage = p.detectors(p.scanID).detStorage;
 %% apply some custom correction such as background subtraction on detStorage.data
 
 if isfield(p.detector, 'bg_sub') && p.detector.bg_sub
-    bg = quantile(detStorage.data, 0.01, 'all');
+    if islogical(p.detector.bg_sub)
+        % subtract anything <0.2% of peak intensity
+        factor = 0.2e-2;
+    else
+        factor = p.detector.bg_sub;
+    end
+    bg = factor * quantile(detStorage.data, 0.99, 'all');
     utils.verbose(2, "Subtracting background intensity (%d)...", bg);
 
     detStorage.data = max(detStorage.data - bg, 0.);
