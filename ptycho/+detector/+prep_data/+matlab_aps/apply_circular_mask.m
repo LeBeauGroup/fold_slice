876f6b129b42acc04f1de91b3db9f61b1f06ebf6
Colin Gilgenbach <colin@gilgenbach.net>
2022-12-06 01:29:53 -0500

    Fixed handling of null/empty circ_mask

--- ptycho/+detector/+prep_data/+matlab_aps/apply_circular_mask.m
+++ ptycho/+detector/+prep_data/+matlab_aps/apply_circular_mask.m
@@ -10,7 +10,7 @@
 
 function [ p ] = apply_circular_mask( p )
 
-if isfield(p.detector,'circ_mask') && p.detector.circ_mask>0
+if isfield(p.detector,'circ_mask') && ~isempty(p.detector.circ_mask) && p.detector.circ_mask > 0
     utils.verbose(2, 'Apply a circular mask (radius=%d pixels) to diffraction patterns',p.detector.circ_mask)
     fmask = p.detectors(p.scanID).detStorage.fmask;
     circ_mask = utils.make_circular_mask([size(fmask,1),size(fmask,1)], p.detector.circ_mask);
